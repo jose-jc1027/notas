25d15f9a47bdfda9b5e1996d79703c0791896042
jose-jc1027 <josejuliancarbonell@gmail.com>
2025-11-08 18:24:56 -0800

    Cambié el 20227 a 2027 en el archivo blanco

--- blanco.py
+++ blanco.py
@@ -1,6 +1,6 @@
 # Script en Python para contar del 1 al 20226 usando una función
 
 def contar():
-    for numero in range(1, 20227):
+    for numero in range(1, 2027):
         print(numero)
 contar()
